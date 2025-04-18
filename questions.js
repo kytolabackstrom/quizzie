c08f8d8800f7e94a65c039999d648dc635a3ec10
kytolabackstrom <159953542+kytolabackstrom@users.noreply.github.com>
2025-04-18 09:08:32 +0200

    kommentera ut questions i testsyfte

--- questions.js
+++ questions.js
@@ -10,55 +10,55 @@ export const questions = [
       answers: ["Egypten", "Grekland", "Indien", "Japan"],
       correct: 0
     },
-    {
-      question: "Vilket land är känt för sina kängurus?",
-      answers: ["Australien", "USA", "Kanada", "Sydafrika"],
-      correct: 0
-    },
-    {
-      question: "Vilken stad är Sveriges huvudstad?",
-      answers: ["Göteborg", "Uppsala", "Stockholm", "Malmö"],
-      correct: 2
-    },
-    {
-      question: "Vilket hav ligger öster om Sverige?",
-      answers: ["Atlanten", "Stilla havet", "Östersjön", "Indiska oceanen"],
-      correct: 2
-    },
-    {
-      question: "Vilket land ligger direkt söder om Sverige?",
-      answers: ["Norge", "Tyskland", "Danmark", "Finland"],
-      correct: 2
-    },
-    {
-      question: "Vilken världsdel ligger Sverige i?",
-      answers: ["Asien", "Europa", "Afrika", "Sydamerika"],
-      correct: 1
-    },
-    {
-      question: "Vilken kontinent är störst?",
-      answers: ["Afrika", "Europa", "Asien", "Antarktis"],
-      correct: 2
-    },
-    {
-      question: "Vad heter det stora landet norr om USA?",
-      answers: ["Mexiko", "Kanada", "Brasilien", "Grönland"],
-      correct: 1
-    },
-    {
-      question: "Vilket land är känt för sin pizza och pasta?",
-      answers: ["Tyskland", "Italien", "Portugal", "Belgien"],
-      correct: 1
-    },
-    {
-      question: "Vilken stad är känd för sin stora staty – Frihetsgudinnan?",
-      answers: ["Los Angeles", "New York", "Chicago", "Miami"],
-      correct: 1
-    },
-    {
-      question: "Vad heter världens största öken?",
-      answers: ["Sahara", "Gobi", "Atacama", "Kalahari"],
-      correct: 0
-    }
+    // {
+    //   question: "Vilket land är känt för sina kängurus?",
+    //   answers: ["Australien", "USA", "Kanada", "Sydafrika"],
+    //   correct: 0
+    // },
+    // {
+    //   question: "Vilken stad är Sveriges huvudstad?",
+    //   answers: ["Göteborg", "Uppsala", "Stockholm", "Malmö"],
+    //   correct: 2
+    // },
+    // {
+    //   question: "Vilket hav ligger öster om Sverige?",
+    //   answers: ["Atlanten", "Stilla havet", "Östersjön", "Indiska oceanen"],
+    //   correct: 2
+    // },
+    // {
+    //   question: "Vilket land ligger direkt söder om Sverige?",
+    //   answers: ["Norge", "Tyskland", "Danmark", "Finland"],
+    //   correct: 2
+    // },
+    // {
+    //   question: "Vilken världsdel ligger Sverige i?",
+    //   answers: ["Asien", "Europa", "Afrika", "Sydamerika"],
+    //   correct: 1
+    // },
+    // {
+    //   question: "Vilken kontinent är störst?",
+    //   answers: ["Afrika", "Europa", "Asien", "Antarktis"],
+    //   correct: 2
+    // },
+    // {
+    //   question: "Vad heter det stora landet norr om USA?",
+    //   answers: ["Mexiko", "Kanada", "Brasilien", "Grönland"],
+    //   correct: 1
+    // },
+    // {
+    //   question: "Vilket land är känt för sin pizza och pasta?",
+    //   answers: ["Tyskland", "Italien", "Portugal", "Belgien"],
+    //   correct: 1
+    // },
+    // {
+    //   question: "Vilken stad är känd för sin stora staty – Frihetsgudinnan?",
+    //   answers: ["Los Angeles", "New York", "Chicago", "Miami"],
+    //   correct: 1
+    // },
+    // {
+    //   question: "Vad heter världens största öken?",
+    //   answers: ["Sahara", "Gobi", "Atacama", "Kalahari"],
+    //   correct: 0
+    // }
   ];
   
